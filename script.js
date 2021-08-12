94203a88dd3ff538642b898d864124705e99f5d1
Robert Gajewski <robert.gajewski@nask.pl>
2021-08-12 22:50:54 +0200

    index.html and scirpt.js update

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ $(document).ready(function() {
 
         $.ajax({
             url: requestUrl + '/' + $.param({
-                taskId: taskId
+                taskId
             }),
             method: 'DELETE',
             success: function() {
